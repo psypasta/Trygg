896433a5f5551d1415ec09d3db1132e5305f5e88
fulsomenko <max.blomstervall@gmail.com>
2017-10-13 00:17:08 +0200

    Tester

--- src/modig/Test.java
+++ src/modig/Test.java
@@ -2,5 +2,15 @@ package modig;
 
 public class Test {
 	
+	public static void main(String args[]) {
+		int a, b ,c = 0;
+		
+		a = 1;
+		b = 3;
+		c = 4;
+		
+		System.out.println(a + b);
+		System.out.println(c);
+	}
 
 }
