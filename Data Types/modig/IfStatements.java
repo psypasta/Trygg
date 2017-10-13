1ce52f115002390c0c0fb68c2f4af5b80e20bfa2
psypasta <robin.van.manen@hotmail.com>
2017-10-13 22:01:29 +0200

    Added itStatement.java, methods n shit

--- Data Types/modig/IfStatements.java
+++ Data Types/modig/IfStatements.java
@@ -4,37 +4,56 @@ public class IfStatements {
 	
 	
 	public static void main(String[] args) {
-		
-		
+
 		boolean gameOver = true;
 		int score = 800;
 		int levelCompleted = 5;
 		int bonus = 100;
 		
-		if(gameOver) {
-			
-
-			int finalScore = score + (levelCompleted * bonus);
-			System.out.println("Your final score was " + finalScore);
-		}
-		/* print out a second score on the screen with the following
-		 * score set to 10000
-		 * levelCompleted set to 8
-		 * bonus set to 200
-		 * but make sure the first printout above still displays as well */
+		int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+		System.out.println("Your final score was: " + highScore);
 		
 		score = 10000;
 		levelCompleted = 8;
 		bonus = 200;
 		
-		if(gameOver) {
-			
+		highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+		System.out.println("Your final score was: " + highScore);
+		
+		displayHighScorePosition();
 
-			int secondFinalScore = score + (levelCompleted * bonus);
-			System.out.println("Your Second Score was: " + secondFinalScore);
+		
+		}
+	public static void displayHighScorePosition(){
+		System.out.println("HIGHSCORE TABLE: ");
+		System.out.println("name: ");
+		System.out.println("Position: ");
+		
+		
+		
+	}
+
+	
+	
+	
+	public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+		
+			if(gameOver) {
+			int finalScore = score + (levelCompleted * bonus);
+			finalScore += 2000;
+			return finalScore;
+			
+			}
+				return -1;
+			
+		}
+			
+			
+	
 		
 	}
 	
-	}
+	
 
-}
+
+	
