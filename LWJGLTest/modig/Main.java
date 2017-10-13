afec5790978a5279410ed0a85f525692ca00f9c0
fulsomenko <max.blomstervall@gmail.com>
2017-10-13 21:21:00 +0200

    CLASSPATH lwjgl test

--- LWJGLTest/modig/Main.java
+++ LWJGLTest/modig/Main.java
@@ -1,10 +1,31 @@
 package modig;
 
+import static org.lwjgl.glfw.GLFW.*;
+
+import org.lwjgl.glfw.GLFWVidMode;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		if(!glfwInit()) {
+			throw new IllegalStateException("Failed to initialize GLFW!");
+		}
 
+		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+		long window = glfwCreateWindow(640, 480, "MYLWJGLProgram", 0, 0);
+		if(window == 0) {
+			throw new IllegalStateException("Failed to create window");
+		}
+		
+		GLFWVidMode videoMode = glfwGetVideoMode(glfwGetPrimaryMonitor());	
+		glfwSetWindowPos(window, (videoMode.width() - 640) / 2, (videoMode.width() - 480) / 2);
+		
+		glfwShowWindow(window);
+		
+		while(!glfwWindowShouldClose(window)) {
+			glfwPollEvents();
+		}
+		
 	}
 
 }
