0cddf083b9691b7dee82fa67d8cfdf2b12c8f121
psypasta <robin.van.manen@hotmail.com>
2017-10-13 14:11:46 +0200

    Run.java edited for SMHI lookup/reader website.

--- src/modig/Editor.java
+++ src/modig/Editor.java
@@ -3,6 +3,7 @@ package modig;
 public class Editor {
 	int lol = 0;
 	int lol1 = 1;
+	int asdf = 3;
 	
 	
 
--- src/modig/Run.java
+++ src/modig/Run.java
@@ -1,25 +1,43 @@
 package modig;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class Run {
 
-	public static void main(String[] args) {
-		Test yolo = new Test();
-		Test robin = new Test(1,2,3,4,5,6);
-		
-		int[] test = new int [6];
-		
-		test = yolo.getVal();
-		
-		for(int i = 0; i < test.length; i++) {
-			System.out.println(test[i]);
-		}
-		
-		test = robin.getVal();
-		
-		for(int i = 0; i < test.length; i++) {
-			System.out.println(test[i]);
-		}
 
+
+	    public static void main(String[] args) throws IOException {
+
+	        // Make a URL to the web page
+	        URL url = new URL("https://www.smhi.se/vadret/vadret-i-sverige/ortsprognoser/q/Göteborg/2711537#tab=0,chart=1");
+
+	        // Get the input stream through URL Connection
+	        URLConnection con = url.openConnection();
+	        InputStream is =con.getInputStream();
+
+	        // Once you have the Input Stream, it's just plain old Java IO stuff.
+
+	        // For this case, since you are interested in getting plain-text web page
+	        // I'll use a reader and output the text content to System.out.
+
+	        // For binary content, it's better to directly read the bytes from stream and write
+	        // to the target file.
+
+
+	        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+	        String line = null;
+
+	        // read each line and write to System.out
+	        while ((line = br.readLine()) != null) {
+	            System.out.println(line);
+	        }
+	    }
 	}
+	
 
-}
+	
