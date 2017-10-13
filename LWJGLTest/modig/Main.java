da1b31cdad8f8e2393433534e22d6b27301e3241
fulsomenko <max.blomstervall@gmail.com>
2017-10-14 01:03:48 +0200

    BLACK WINDOW

--- LWJGLTest/modig/Main.java
+++ LWJGLTest/modig/Main.java
@@ -1,8 +1,10 @@
 package modig;
 
 import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
 
 import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
 
 public class Main {
 
@@ -11,11 +13,19 @@ public class Main {
 			throw new IllegalStateException("Failed to initialize GLFW!");
 		}
 
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+		
 		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
 		long window = glfwCreateWindow(640, 480, "MYLWJGL Program", 0, 0);
 		if(window == 0) {
 			throw new IllegalStateException("Failed to create window");
 		}
+		
+		glfwMakeContextCurrent(window);
+		GL.createCapabilities();
 		
 		GLFWVidMode videoMode = glfwGetVideoMode(glfwGetPrimaryMonitor());	
 		glfwSetWindowPos(window, (videoMode.width() - 640) / 2, (videoMode.height() - 480) / 2);
@@ -24,6 +34,10 @@ public class Main {
 		
 		while(!glfwWindowShouldClose(window)) {
 			glfwPollEvents();
+			
+			glClear(GL_COLOR_BUFFER_BIT);
+			
+			glfwSwapBuffers(window);
 		}
 		
 		glfwTerminate();
