45f0bb1e6a2398a655daab9653f7e993c132ed1a
psypasta <robin.van.manen@hotmail.com>
2017-10-16 10:39:24 +0200

    sup bro ifstatements inc

--- Data Types/modig/IfStatements.java
+++ Data Types/modig/IfStatements.java
@@ -20,20 +20,58 @@ public class IfStatements {
 		highScore = calculateScore(gameOver, score, levelCompleted, bonus);
 		System.out.println("Your final score was: " + highScore);
 		
-		displayHighScorePosition();
-
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		int highScorePosition = calculateHighScoreposition(1000);
+		displayHighScorePosition("Robin", highScorePosition);
+		
+		highScorePosition = calculateHighScoreposition(900);
+		displayHighScorePosition("Tim", highScorePosition);
+		
+		highScorePosition = calculateHighScoreposition(400);
+		displayHighScorePosition("Percy", highScorePosition);
+		
+		highScorePosition = calculateHighScoreposition(50);
+		displayHighScorePosition("Gilbert", highScorePosition);
+		
+		
 		
 		}
-	public static void displayHighScorePosition(){
-		System.out.println("HIGHSCORE TABLE: ");
-		System.out.println("name: ");
-		System.out.println("Position: ");
+	public static void displayHighScorePosition(String playerName, int highScorePosition){
+		
+		
+		System.out.println(playerName + " managed to get into position: " + highScorePosition + ", on the high score table ");
 		
 		
 		
 	}
 
-	
+	public static int calculateHighScoreposition(int playerScore) {
+		
+		if(playerScore >= 1000) {
+			return 1;
+			
+		}else if (playerScore >= 500 && playerScore < 1000 ) {
+			return 2;
+				
+			}else if (playerScore >= 100 && playerScore < 500) {
+				return 3;
+				
+			}else {
+				return 4;
+				
+			}
+		
+		
+	}
 	
 	
 	public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
