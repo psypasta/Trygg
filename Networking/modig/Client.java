2105c2bc9c2ae8fbac24a840c650493c7a57bdde
mjaok <mjaok@LAPTOP-3M133SS3.lan>
2017-10-16 15:50:42 +0200

    Wtf

---
+++ Networking/modig/Client.java
@@ -0,0 +1,40 @@
+package modig;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+	public static void main(String[] args) {
+
+		try(Socket socket = new Socket("192.168.10.125", 5555)){
+			
+			BufferedReader echoes = new BufferedReader(
+					new InputStreamReader(socket.getInputStream()));
+			PrintWriter stringToEcho = new PrintWriter(socket.getOutputStream(), true);
+			
+			Scanner scanner = new Scanner(System.in);
+			String echoString;
+			String response;
+			
+			do {
+				System.out.println("Enter string to be echoed");
+				echoString = scanner.nextLine();
+				
+				stringToEcho.println(echoString);
+				if(!echoString.equals("exit")) {
+					response = echoes.readLine();
+					System.out.println(response);
+				}
+			} while(!echoString.equals("exit"));
+			
+		}catch(IOException e){
+			System.out.println("Client error: " + e.getMessage());
+		}
+
+	}
+
+}
