52847602ac65ba1e05b795cd7ce4ac4e32775917
Robin van Manen <Robin van Manen@LAPTOP-NG6RPG69.lan>
2017-10-16 11:46:25 +0200

    added rainbowtrout and sockeyesalmon

---
+++ Animal/modig/RainbowTrout.java
@@ -0,0 +1,10 @@
+package modig;
+
+public class RainbowTrout extends Fish {
+
+	public RainbowTrout(String k, String a) {
+		super("rainbowtrout", "bands of brilliant speckled multicolored stripes running nearly the whole length of its body");
+	
+	}
+
+}
---
+++ Animal/modig/SockeyeSalmon.java
@@ -0,0 +1,10 @@
+package modig;
+
+public class SockeyeSalmon extends Fish {
+
+	public SockeyeSalmon(String k, String a) {
+		super("sockeyesalmon", "bright red with a green head");
+		
+	}
+
+}
