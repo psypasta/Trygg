1583fd5a7a1b6fba3063f2fb297aca0cc3063d58
mjaok <mjaok@LAPTOP-3M133SS3.lan>
2017-10-16 11:47:05 +0200

    Added AmericanRobin and DomesticCanary class

---
+++ Animal/modig/AmericanRobin.java
@@ -0,0 +1,8 @@
+package modig;
+
+public class AmericanRobin extends Bird{
+
+	public AmericanRobin() {
+		super("americanrobin", "red breast");
+	}
+}
---
+++ Animal/modig/DomesticCanary.java
@@ -0,0 +1,10 @@
+package modig;
+
+public class DomesticCanary extends Bird{
+
+	public DomesticCanary() {
+		super("domesticcanary", "yellow, orange, black, " + 
+				"brown, white, red");
+	}
+
+}
