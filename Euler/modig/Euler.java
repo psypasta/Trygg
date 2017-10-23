42c6433ed961cac2d4dc6ddb183140aaf06cf587
mjaok <mjaok@LAPTOP-3M133SS3>
2017-10-24 00:27:10 +0200

    Problem 8 WIP

--- Euler/modig/Euler.java
+++ Euler/modig/Euler.java
@@ -66,8 +66,111 @@ public class Euler {
 		System.out.println(primeList.size() + " prime, is: " + primeList.get(primeList.size()-1));
 		
 		//ID7 end
+		
+		//ID8
+		
+		String thousandN = "73167176531330624919225119674426574742355349194934" + 
+							"96983520312774506326239578318016984801869478851843" + 
+							"85861560789112949495459501737958331952853208805511" + 
+							"12540698747158523863050715693290963295227443043557" + 
+							"66896648950445244523161731856403098711121722383113" + 
+							"62229893423380308135336276614282806444486645238749" + 
+							"30358907296290491560440772390713810515859307960866" + 
+							"70172427121883998797908792274921901699720888093776" + 
+							"65727333001053367881220235421809751254540594752243" + 
+							"52584907711670556013604839586446706324415722155397" + 
+							"53697817977846174064955149290862569321978468622482" + 
+							"83972241375657056057490261407972968652414535100474" + 
+							"82166370484403199890008895243450658541227588666881" + 
+							"16427171479924442928230863465674813919123162824586" + 
+							"17866458359124566529476545682848912883142607690042" + 
+							"24219022671055626321111109370544217506941658960408" + 
+							"07198403850962455444362981230987879927244284909188" + 
+							"84580156166097919133875499200524063689912560717606" + 
+							"05886116467109405077541002256983155200055935729725" + 
+							"71636269561882670428252483600823257530420752963450";
+		System.out.println(thousandN);
+		
+		int range = 4;
+	//	ArrayList<Integer> products = new ArrayList<Integer>();
+		int[] products = new int[1000];
+		
+		for(int i = 0; i < products.length; i++) {
+			products[i] = 1;
+		//	System.out.println(products[i]);
+		} 
+		
+		int c = 1;
+		String b = null;
+		
+		String[] digits = new String[1000];
+	//	char[] fours = new char[4];
+	//	String[] subString = new String[4];
+	//	System.out.println(thousandN.substring(range-range, range));
+		
+		for(int i = 0; i < thousandN.length()-3; i++) {
+			//a = thousandN.substring(range-range, range);
+			
+			b = thousandN.substring(range-range+i, range+i);
+			digits[i] = b;
+		//	System.out.println(digits[i]);
+		}
+		
+		/*c =  Character.getNumericValue(digits[1].charAt(0)) * 
+			 Character.getNumericValue(digits[1].charAt(1)) * 
+			 Character.getNumericValue(digits[1].charAt(2)) *
+			 Character.getNumericValue(digits[1].charAt(3)); 
+		System.out.println(c);
+		System.out.println(digits[1]);
+		System.out.println(digits[1].charAt(0));
+		System.out.println(digits[1].length());
+		System.out.println(	Character.getNumericValue(digits[1].charAt(0))*
+							Character.getNumericValue(digits[1].charAt(1))*
+							Character.getNumericValue(digits[1].charAt(2))*
+							Character.getNumericValue(digits[1].charAt(3))); */
+		
+		for(int i = 0; i < digits.length-3; i++) {
+			for(int j = 0; j < digits[i].length(); j++) {
+				products[i] *= Character.getNumericValue(digits[i].charAt(j));
+				System.out.println(i + "<- i " + "j ->" + j);
+				
+			/*	if(products[i] == 0) {
+					System.out.println(products[i] + "Location: " + i);
+				}
+				else {
+					System.out.println(products[i]
+				} */
+			//	products[i] = (digits[i] * digits[j]);
+			//	System.out.println(products[i]);
+			//	System.out.println(products[i]);
+			}
+			if(products[i] == 0) {
+				System.out.println(products[i] + " " + "Location: " + i);
+				//largest @615 5832
+			}
+			else {
+				System.out.println(products[i]);
+			}
+			if(i == 987) {
+				break;
+			}
+		} 
+		maxN(products);
+			
 	}
 	
+	public static int maxN(int[] n) {
+		
+		 int max = n[0];
+
+	        for (int i = 1; i < n.length; i++){
+	        	if (n[i] > max){
+	        		max = n[i];
+	        	}
+	        }        
+	        System.out.println("The highest maximum is: " + max);
+		return 0;
+	}
 	//ID 6
 	
 	public static int squareSum(int n) {
@@ -119,6 +222,11 @@ public class Euler {
 		}
 		return maxPalin;
 	}
+
+	
+	//ID4 end
+	
+	// Project euler ID 3 
 	
 	public static boolean isPalindrome(int a) {
 		String value = null;
@@ -133,11 +241,7 @@ public class Euler {
 		  
 		  return true;
 	}
-	
-	//ID4 end
-	
-	// Project euler ID 3 
-	
+
 	public static ArrayList<Long> prime_factors(long n) {
 
 		ArrayList<Long> factors = new ArrayList<Long>(1);
