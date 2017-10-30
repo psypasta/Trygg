c59fb196bc9ac644a17467c7d2d7be356af305f8
psypasta <robin.van.manen@hotmail.com>
2017-10-30 20:21:21 +0100

    added some shit

--- Cashier/Cashier/User.java
+++ Cashier/Cashier/User.java
@@ -1,5 +1,24 @@
 package Cashier;
 
 public class User {
+	
+	private String name;	
+	private int code;
+	
+	
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 
 }
