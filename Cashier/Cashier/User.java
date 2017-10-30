fbf94e128123a9aef6e299ca606390a17e626182
fulsomenko <max.blomstervall@gmail.com>
2017-10-30 20:24:19 +0100

    constructor user

--- Cashier/Cashier/User.java
+++ Cashier/Cashier/User.java
@@ -5,6 +5,10 @@ public class User {
 	private String name;	
 	private int code;
 	
+	public User(String name, int code) {
+		this.name = name;
+		this.code = code;
+	}
 	
 	public int getCode() {
 		return code;
