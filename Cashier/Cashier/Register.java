2db27beb7802a210144ad1aa4de784746a51bcab
fulsomenko <max.blomstervall@gmail.com>
2017-10-31 18:16:21 +0100

    hjälp

--- Cashier/Cashier/Register.java
+++ Cashier/Cashier/Register.java
@@ -8,9 +8,16 @@ public class Register {
 	//product count
 	private int productCount;
 	private List <Product> productList = new ArrayList<Product>();
-	private Receipt kvitto = new Receipt(productList);
+	//private Receipt kvitto = new Receipt(productList);
+	private Sale s = new Sale(productList);
 	
+	public void addProduct() {
+		
+	}
 	
+	public Receipt commitSale() {
+		return s.getReceipt();
+	}
 	
 	public double weigh() {
 		return 0;
--- Cashier/Cashier/Sale.java
+++ Cashier/Cashier/Sale.java
@@ -1,7 +1,45 @@
 package Cashier;
 
+import java.util.List;
+
 public class Sale {
 	private int ID;
 	private String Date;
 	private final double MOMS = 0.25;
+	private Payment payment;
+	private Receipt kvitto;
+	
+	public Sale(List productList) {
+		kvitto = new Receipt(productList);
+		getSubTotal(productList);
+	}
+	
+	public Receipt getReceipt() {
+		return kvitto;
+	}
+	
+	public double getTax() {
+		return 0;
+	}
+	
+	public double getSubTotal(List<Product> productList) {
+		
+		for(int i = 0; i < productList.size(); i++) {
+			System.out.println(productList.get(i).getPrice()); //hjälp
+		}
+		
+		return 0;
+	}
+	
+	public double getTotal() {
+		
+		
+		
+		return 0;
+	}
+	
+	public void setPayment() {
+		
+	}
+	
 }
