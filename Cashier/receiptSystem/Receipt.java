86832f82ab302be76edaf5ce2be35b471aa3c543
psypasta <robin.van.manen@hotmail.com>
2017-11-01 14:35:30 +0100

    made some minor changes to receipt, not working, trying to add dateformat scheme

--- Cashier/receiptSystem/Receipt.java
+++ Cashier/receiptSystem/Receipt.java
@@ -1,10 +1,12 @@
 package receiptSystem;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 import registerSystem.Product;
-
+import java.util.Date;
 public class Receipt {
 	
 	private static final double MOMS = 0.25;
@@ -12,15 +14,25 @@ public class Receipt {
 	
 	private String CompanyName;
 	private int Orgnumber;
-	private String date;
+	private String Date;
 	private List <Product> productList = new ArrayList<Product>();
-
+	
 	public Receipt(List productList) {
 		this.productList = productList;
+		CompanyName = "test";
+		Orgnumber = 1337;
+		
 	}
 	
 	public void print() {
 		
 	}
-	
+	public class GetCurrentDateTime
+	{
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
+		
+		
+		
+	}
 }
