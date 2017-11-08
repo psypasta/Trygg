c6e647d42d7ba381c23094a4ccc05d42e47fd647
fulsomenko <max.blomstervall@gmail.com>
2017-11-08 23:05:19 +0100

    Changed productFactory to get object info from productsTest.txt

--- Cashier/adminSystem/FileFactory/ProductFactory.java
+++ Cashier/adminSystem/FileFactory/ProductFactory.java
@@ -39,10 +39,14 @@ public class ProductFactory implements FileFactory {
 	@Override
 	public String[] fileArray() {
 		FileHandler uh = new FileHandler();
-		String userFile = uh.read("products.txt");
+		String userFile = uh.read("productsTest.txt");
 		
-		String[] lines = userFile.split(System.getProperty("line.separator"));
-			
+		//System.getProperty("line.separator"); funkade med products.txt, men inte productsTest.txt
+		/*
+		 * If you don’t want empty lines:
+		 * String.split("[\\r\\n]+") tänk på detta max :)
+		 */
+		String[] lines = userFile.split("\\r?\\n");
 		return lines;
 	}
 	
@@ -50,7 +54,6 @@ public class ProductFactory implements FileFactory {
 	public String[] arrayChop(String[] lines, int index) {
 		
 		String[] test = new String[4];
-		
 		test = lines[index].split("\\s+");
 		
 	/*	ID = 0;
@@ -60,20 +63,36 @@ public class ProductFactory implements FileFactory {
 		
 		for(int j = 0; j < test.length; j++) {
 			if(j == 0) {
+				ID = Integer.parseInt(test[j]);
+			}
+			else if(j == 1) {
+				productName = test[j];
+			}else if(j == 2) {
+				price = Double.parseDouble(test[j]);
+			}
+			else if(j == 3) {
+				category = test[j];
+			}
+		/*
+		 * products.txt
+			if(j == 0) {
 				category = test[j];
 				
-			}else if(j == 1) {
+			}
+			else if(j == 1) {
 				//wtf varför är denna unit? Hur kan arrayen bli 5 stor? när jag deklarerat den 4 på rad 52?
 				System.out.println(j + " index " + test[j]);
-			}else if(j == 2) {
+			}
+			else if(j == 2) {
 				ID = Integer.parseInt(test[j]);
 			}else if(j == 3) {
 				productName = test[j];
 			}
 			else if(j == 4) {
 				price = Double.parseDouble(test[j]);
-			}
+			}*/
 		} 
+		
 		return test;
 	}
 
