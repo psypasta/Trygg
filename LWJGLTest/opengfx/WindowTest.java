0c3e2d093fc9aa9333ba33935a29777b30a857ac
fulsomenko <max.blomstervall@gmail.com>
2017-12-11 21:25:20 +0100

    comment

--- LWJGLTest/opengfx/WindowTest.java
+++ LWJGLTest/opengfx/WindowTest.java
@@ -11,7 +11,8 @@ public class WindowTest {
 		GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MAJOR, 3);
 		GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MINOR, 3);
 		GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_PROFILE, GLFW.GLFW_OPENGL_CORE_PROFILE);
-	    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+		//this line is for mac
+		//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 		    
 		
 	}
