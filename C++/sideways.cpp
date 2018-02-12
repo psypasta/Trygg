98573e465f1470d97405a8ec77702775824f217e
mb <max.blomstervall@gmail.com>
2018-02-12 22:04:23 +0100

    trekant

--- C++/sideways.cpp
+++ C++/sideways.cpp
@@ -1,25 +1,53 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unistd.h>
 
 using namespace std;
 
+vector<string> triToVec(int);
 vector<string> quadToVec(int);
 string lineMake(int);
 string spaceMake(int, int);
 
 int main(){
 
-	for(int i = 0; i < 5; i++){
-		vector<string> v;
-		v = quadToVec(10);
+//	while(true){
 
-	    for(string n : v) {
-	    	n += n + n;
-	        cout << n << '\n';
+		for(int i = 0; i < 1; i++){
+			vector<string> v;
+			v = quadToVec(15);
+
+			vector<string> triVec;
+			triVec = triToVec(20);
+
+		    for(string n : v) {
+		    //	n = n + n;
+		        cout << n << '\n';
+			}  
+
+			for(string n : triVec) {
+		    //	n = n + n;
+		        cout << n << '\n';
+			}
 		}
+/*		unsigned int microseconds = 6000;
+		usleep(microseconds);
+
+		printf("\033c");
 	}
+*/
 	return 0;
+}
+
+vector<string> triToVec(int size){
+	vector<string> tri;
+	
+	for(int i = size; i > size-size; i--){
+		tri.push_back(spaceMake(i, size) + lineMake(i) + lineMake(i) + spaceMake(i, size));
+	}
+
+	return tri;
 }
 
 vector<string> quadToVec(int size){
