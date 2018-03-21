9fc65c581e000325dd2aedc6c6aa6c61a055a7db
max <max@idea-PC>
2018-03-21 20:58:24 +0100

    Attacks 2

--- YoloGenerator/modig/Soldier.java
+++ YoloGenerator/modig/Soldier.java
@@ -2,7 +2,16 @@ package modig;
 
 public abstract class Soldier extends Unit{
 
-	public abstract void attack();
-	public abstract void block();
-	
+	public int attack(Unit u) {
+		// TODO Auto-generated method stub
+		this.stamina = this.stamina - 10;
+		return 10;
+	}
+
+	public void block(int d) {
+		if(this.stamina < 50) {
+			this.health = this.health - d;
+		}
+		
+	}
 }
