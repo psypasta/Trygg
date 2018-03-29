43b6335d9039350f7923c82b2c1ee68a802991f6
psypasta <robin.van.manen@hotmail.com>
2018-03-29 17:32:38 +0200

    Ability to create AccountLists

--- Bank/bank/view/CreateAccount.java
+++ Bank/bank/view/CreateAccount.java
@@ -5,25 +5,31 @@ import java.util.List;
 
 import bank.transfer.Account;
 import bank.transfer.Customer;
+import bank.util.FileGet;
 
 public class CreateAccount {
 	
-	
+	private List <Account> createAccount;
 	private String accountNumber;
 	private String accountName;
 	private Customer accountOwner;
 	private double accountBalance;
 	
-	
-	Account account;
-	
-	private List <Account> CreateAccount = new ArrayList<Account>();
+	public CreateAccount() {
+		FileGet file = new FileGet();
 		
+		createAccount =
+		file.accountList();
+		
+	}
+	//Account account;
+	
+	
 		
 	//
 	
-	public List <Account> Create(){
-		return this.CreateAccount;
+	public List <Account> getAccountList(){
+		return this.createAccount;
 	}
 		
 	
